169d256a6cbdae739062eef8c5ba51129ca1007e
vinay <vinayverma158@gmail.com>
2024-08-07 15:52:22 +0530

    dsfgf

--- backend/config/mysql_database.js
+++ backend/config/mysql_database.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise');
 
 const mysqlPool = mysql.createPool({
     host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'nodejs',
+    user: 'dfwebsol_nodejs',
+    password: 'dfwebsol_df_nodejs',
+    database: '~e6ZF@Z.%^k(04flk%@$1uQs',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
